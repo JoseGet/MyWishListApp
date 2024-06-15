ddca54370901fd29bd3dad3213daa244880e4bda
Jose Getulio <getuliojose96@gmail.com>
2024-06-15 16:01:20 -0400

    Adicao da Tela de adicao ou edicao de novos items e primeira etapa de definicao do ViewModel

--- app/src/main/java/com/example/mywishlistproject/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistproject/data/Wish.kt
@@ -9,8 +9,8 @@ data class Wish(
 object DummyWish{
     val wishList = listOf(
         Wish(title = "Samsung Galaxy S23 5G", description = "Korean Smartphone"),
-        Wish(title = "Samsung Galaxy S24 5G", description = "Korean Smartphone"),
-        Wish(title = "Samsung Galaxy S22 5G", description = "Korean Smartphone"),
+        Wish(title = "Samsung Galaxy S24 Ultra", description = "Korean Smartphone"),
+        Wish(title = "Samsung Galaxy S22 +", description = "Korean Smartphone"),
         Wish(title = "Iphone 15 PRO MAX", description = "Apple Smartphone")
 
     )
